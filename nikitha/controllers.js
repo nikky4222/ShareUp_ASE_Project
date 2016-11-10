df250e8c497afc1f0e7ac47462dccc9aa8c79668
nikky4222 <nikky4222@gmail.com>
2016-11-09 22:49:25 -0600

    commit

--- nikitha/controllers.js
+++ nikitha/controllers.js
@@ -11,55 +11,135 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
   };
 })
-  .controller('LoginCtrl', function ($scope, $state,LoginService, $firebaseAuth,$ionicPopup,$cookieStore) {
-
-    var fbAuth = $firebaseAuth();
-
-    $scope.logi = function(username, password) {
-      fbAuth.$signInWithEmailAndPassword(username, password).then(function (authData) {
-        $state.go("home");
-      }).catch(function (error) {
-        console.error("ERROR: " + error);
-      });
-    };
-  })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
 
-  .controller('HomeCtrl', function($scope, $stateParams) {
+  .controller('homeCtrlr', function($scope, $stateParams, Chats) {
   })
 
-  .controller('MainCtrl', function($scope, $state) {
-    $scope.login1 = function() {
-      $state.go('login');
+
+
+  .controller('groupCtrlr', function($scope,$http) {
+    $scope.data1={};
+    $scope.gnamename=[];
+    $scope.getusers=function () {
+      var gname=$scope.data1.group;
+      //console.log($scope.data1.group)
+      var name1=$http.get("https://api.mongolab.com/api/1/databases/harry/collections/users?q={\"gname\":\""+gname+"\"}&apiKey=0xCX7-4KL6dGWvAOR5PLzPaC-DtA0KZ4")
+      name1.success(function(data){
+console.log(data.length)
+        for(var i=0;i<data.length;i++) {
+          $scope.gnamename[i] = {"name": data[0].username};
+        }
+        console.log($scope.gnamename)
+      });
     }
 
-    $scope.register1 = function() {
+
+
+  })
+
+
+
+
+  .controller('loginCtrl', function($scope, LoginService, DeleteService, UpdateService, $ionicPopup, $state) {
+      $scope.data = {};
+
+
+
+      $scope.login = function(username) {
+        LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+          alert("success");
+          $state.go('register');
+        }).error(function(data) {
+          var alertPopup = $ionicPopup.alert({
+            title: 'Login failed!',
+            template: 'Please check your credentials!'
+          });
+        });
+
+      }
+    $scope.redirectToRegistration=function()
+    {
       $state.go('register');
+    };
+
+
+      $scope.goToHome =function()
+      {
+
+
+        $state.go('register');
+      }
+
+
+
     }
-
-
-  })
-
-
-  .controller('RegCtrl', function($scope, $stateParams) {
-  })
+  )
 
 
 
-.controller('AccountCtrl', function($scope) {
+
+
+  .controller('registrationCtrlr', function($scope, RegisterService, DeleteService, UpdateService, $ionicLoading,$ionicPopup, $state) {
+      $scope.data = {};
+
+      $scope.delete =function(username)
+      {
+
+
+        DeleteService.deleteUser($scope.data.username).success(function(data) {
+          var alertPopup = $ionicPopup.alert({
+            title: 'Deleted!',
+            template: 'Your account is deleted succesfully!'
+          });
+        }).error(function(data) {
+          var alertPopup = $ionicPopup.alert({
+            title: 'Login failed!',
+            template: 'Please check your credentials!'
+          });
+        });
+      }
+      $scope.update =function(username)
+      {
+        UpdateService.updateUser($scope.data.username).success(function(data) {
+          var alertPopup = $ionicPopup.alert({
+            title: 'Updated!',
+            template: 'Your account is updated succesfully!'
+          });
+        }).error(function(data) {
+          var alertPopup = $ionicPopup.alert({
+            title: 'Update failed!',
+            template: 'Please check your credentials!'
+          });
+        });
+      }
+
+      $scope.completeRegistration = function(email){
+
+        RegisterService.RegisterUser( $scope.data.username, $scope.data.password,$scope.data.cpassword, $scope.data.mobile,$scope.data.nick,$scope.data.gname).success(function(data) {
+          alert("Welcome " +$scope.data.nick  );
+          $state.go('login');
+        }).error(function(data) {
+          var alertPopup = $ionicPopup.alert({
+            title: 'Registration failed!',
+            template: 'Please check your credentials!'
+          });
+        });
+      }
+
+})
+
+  .controller('AccountCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
   };
-
-
 });
